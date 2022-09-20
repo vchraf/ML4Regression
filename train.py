3b8db29f6e0fc6140db021bec7d85bb27f24f9c2
vchraf <vchraf@gmail.com>
2022-09-21 00:41:26 +0100

    fix the connection between train file and MLFlow

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
@@ -12,18 +14,11 @@ import mlflow.sklearn
 from mlflow.tracking import MlflowClient
 from mlflow.entities import ViewType
 
-mlflow.set_tracking_uri(parameters.TRACKING_URL)
-mlflow.set_experiment(parameters.EXP_NAME)
-client = MlflowClient(tracking_uri=mlflow.get_tracking_uri())
-
-EXP_ID=dict(mlflow.get_experiment_by_name(parameters.EXP_NAME))['experiment_id']
-run = client.search_runs(experiment_ids=EXP_ID, run_view_type=ViewType.ACTIVE_ONLY, max_results=1, order_by=["metrics.r2  DESC"])[0]
-model_params = run.data.params
-for key in model_params.keys():
-    try:
-        model_params[key] = ast.literal_eval(model_params[key])
-    except : pass
-
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
 def load_data(src):
     data = pd.read_parquet(src)
     train, test = train_test_split(data)
@@ -32,25 +27,34 @@ def load_data(src):
     X_test = test.drop(["price"], axis=1)
     y_train = train[["price"]]
     y_test = test[["price"]]
-    return X_train, X_test, y_train, y_test 
+    return X_train, X_test, y_train, y_test
 
 
-def eval_metrics(actual, pred):
-    rmse = np.sqrt(mean_squared_error(actual, pred))
-    mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred)
-    return rmse, mae, r2
+mlflow.set_tracking_uri(parameters.TRACKING_URL)
+mlflow.set_experiment(parameters.EXP_NAME)
 
+
+# client = MlflowClient(tracking_uri=mlflow.get_tracking_uri())
+
+# EXP_ID=dict(mlflow.get_experiment_by_name(parameters.EXP_NAME))['experiment_id']
+# run = client.search_runs(experiment_ids=EXP_ID, run_view_type=ViewType.ACTIVE_ONLY, max_results=1, order_by=["metrics.r2  DESC"])[0]
+# model_params = run.data.params
+# for key in model_params.keys():
+#     try:
+#         model_params[key] = ast.literal_eval(model_params[key])
+#     except : pass
+
+
+
+X_train, X_test, y_train, y_test = load_data(src=parameters.DATASET)
 
 with mlflow.start_run():
-    model = parameters.MODEL(**model_params)
-
+    model = parameters.MODEL()
+    print(model.__class__.__name__)
     mlflow.set_tag("modele", model.__class__.__name__)
     mlflow.set_tag("status", "final")
-    mlflow.log_params(model_params)
-    X_train, X_test, y_train, y_test = load_data(src=parameters.DATASET)
-
-
+    # mlflow.log_params(best_params)
+    
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     (rmse, mae, r2) = eval_metrics(y_test, y_pred)
@@ -61,5 +65,5 @@ with mlflow.start_run():
     mlflow.log_metric("rmse", rmse)
     mlflow.log_metric("r2", r2)
     mlflow.log_metric("mae", mae)
-    mlflow.sklearn.log_model(model, "model", registered_model_name=parameters.MODEL.__class__.__name__)
-#     mlflow.log_artifact(local_path="models/rf_reg.bin", artifact_path="models_pickle")
+    mlflow.sklearn.log_model(model, "model", registered_model_name=model.__class__.__name__)
+
